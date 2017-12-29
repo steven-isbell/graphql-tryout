9688e28fb53ccaebcecee4bbfe3fc15055732218
Steven Isbell <steven.isbell18@gmail.com>
2017-12-28 21:13:32 -0600

    added styled-components

--- src/components/Quotes/QuotesContainer.js
+++ src/components/Quotes/QuotesContainer.js
@@ -1,14 +1,36 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import axios from "axios";
+import styled from "styled-components";
 import QuoteDisplay from "./QuoteDisplay/QuoteDisplay";
+
+const FlexedWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`;
+
+const CardWrapper = styled.div`
+  height: 200px;
+  background: #f0f0f0;
+  margin: 10px;
+  padding: 10px;
+  max-width: 350px;
+`;
+
+const InputWrapper = styled.div``;
 
 class QuotesContainer extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      quotes: []
+      quotes: [],
+      filterVal: ""
     };
+
+    this.handleFilterChange = this.handleFilterChange.bind(this);
   }
 
   componentDidMount() {
@@ -18,13 +40,38 @@ class QuotesContainer extends Component {
       .catch(console.log);
   }
 
+  handleFilterChange(val) {
+    this.setState({ filterVal: val });
+  }
+
   render() {
-    const { quotes } = this.state;
+    const { quotes, filterVal } = this.state;
     return (
-      <div>
-        {quotes.length > 0 &&
-          quotes.map((quote, i) => <QuoteDisplay quote={quote} key={i} />)}
-      </div>
+      <Fragment>
+        <label htmlFor="filter_input">Text Filter &nbsp;</label>
+        <input
+          id="filter_input"
+          type="text"
+          placeholder="Search Name or Text"
+          onChange={e => this.handleFilterChange(e.target.value)}
+        />
+        <FlexedWrapper>
+          {quotes.length > 0 &&
+            quotes
+              .filter(
+                quote =>
+                  quote.quote.includes(filterVal) ||
+                  quote.character.includes(filterVal)
+              )
+              .map((quote, i) => (
+                <CardWrapper>
+                  <FlexedWrapper>
+                    <QuoteDisplay quote={quote} key={i} />
+                  </FlexedWrapper>
+                </CardWrapper>
+              ))}
+        </FlexedWrapper>
+      </Fragment>
     );
   }
 }
